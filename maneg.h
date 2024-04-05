e92a6ead1a6fc1b9d4a19d7610c9d0f2c35e4cd7
Maksim Golovkin <118381732+Mxaqq@users.noreply.github.com>
2024-04-05 06:41:25 +0300

    Update maneg.h

--- maneg.h
+++ maneg.h
@@ -1,24 +1,22 @@
 #pragma once
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include "tablichka.h"
+
 #include "LinearOnArray.h"
 #include "LinearOnList.h"
 #include "OrderedOnArray.h"
 #include "Tree.h"
 #include "HashList.h"
 #include "HashChain.h"
+
 #include <map>
+
 using namespace std;
 
-/*!
-* Âûïîëíèë: Ìàêàäðàé À.
-* Êëàññ ÿ÷åéêè äëÿ ñïèñêà
-*/
-
-struct Line
-{
+struct Line {
 	string key;
 	TPolinom value;
 };
@@ -38,76 +36,85 @@ public:
 		temp.emplace("HashChain", new HashChain<string, TPolinom>);
 		tec = temp["LinerOnArray"];
 	}
+
 	bool Insert(string key, TPolinom value);
 	bool Delete(string key);
-	TPolinom* Find(string key); 
+	TPolinom* Find(string key);
 	bool ChooseTable(string name);
-	friend ostream& operator<<(ostream& os, maneger& tab)
-	{
+
+	friend ostream& operator<<(ostream& os, maneger& tab) {
 		cout << (*tab.tec);
 		return os;
 	}
+
 	class iterator {
 		table<string,TPolinom>* tec;
 	public:
-		iterator(maneger men) {	
+		iterator(maneger men) {
 			tec = men.tec;
 			tec->Reset();
 		}
+
 		Line operator*() {
 			Line temp;
 			temp.key = tec->GetKey();
 			temp.value = tec->GetValuePtr();
 			return temp;
 		}
+
 		void operator++(int n) {
 			tec->GoNext();
 		}
+
 		bool end() {
 			return tec->IsTabEnded();
 		}
 	};
-	int size() { return tec->Count(); }
 
+	int size() { return tec->Count(); }
 };
 
 bool maneger::Insert(string key, TPolinom value) {
 	auto it = temp.begin();
 	bool flag = true;
+
 	while (flag && it != temp.end()) {
 		flag = (it->second->Insert(key, value) && flag);
 		it++;
 	}
+
 	return flag;
 }
 
-inline bool maneger::Delete(string key)
-{
+inline bool maneger::Delete(string key) {
 	auto it = temp.begin();
 	bool flag = true;
+
 	while (flag && it != temp.end()) {
 		flag = (it->second->Delete(key) && flag);
 		it++;
 	}
+
 	return flag;
 }
 
-inline TPolinom* maneger::Find(string key)
-{
+inline TPolinom* maneger::Find(string key) {
 	return tec->Find(key);
 }
 
-inline bool maneger::ChooseTable(string name)
-{
+inline bool maneger::ChooseTable(string name) {
 	auto it = temp.begin();
 	bool flag = false;
+
 	while (!flag && it != temp.end()) {
 		if (it->first == name) {
 			flag = true;
 			tec = it->second;
 		}
+
 		it++;
 	}
+
 	return flag;
 }
 
